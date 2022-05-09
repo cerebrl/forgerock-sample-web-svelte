72a2770672332d3854ff6e9fef9e1a1cd6b834b9
Justin Lowery <justin.lowery@forgerock.com>
2022-05-09 18:38:52 -0500

    task(scan): prepare for security scan test

---
+++ rollup.config.js
@@ -0,0 +1,80 @@
+import alias from '@rollup/plugin-alias';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import svelte from 'rollup-plugin-svelte';
+import preprocess from 'svelte-preprocess';
+
+export default {
+  input: 'src/lib/main.ts',
+  output: {
+    dir: 'scan',
+    // file: 'public/bundle.js',
+    // format: 'es'
+    preserveModules: true
+    // sourcemap: false,
+  },
+  plugins: [
+    // Aliases to help Rollup find the paths declared in tsconfig.json
+    // Unfortunately, this breaks the importing of svelte files
+    alias({
+      entries: {
+        $icons: 'src/lib/components/icons',
+        $journey: 'src/lib/components/journey',
+        $lib: 'src/lib',
+        $todos: 'src/lib/components/todos',
+        $utilities: 'src/lib/components/utilities'
+      }
+    }),
+    resolve({ browser: true }),
+    svelte({
+      // By default, all ".svelte" files are compiled
+      // extensions: ['.my-custom-extension'],
+
+      // You can restrict which files are compiled
+      // using `include` and `exclude`
+      include: 'src/**/*.svelte',
+
+      // Optionally, preprocess components with svelte.preprocess:
+      // https://svelte.dev/docs#svelte_preprocess
+      // preprocess: {
+      //   style: ({ content }) => {
+      //     return transformStyles(content);
+      //   }
+      // },
+      preprocess: preprocess({
+        typescript: {
+          tsconfigFile: './tsconfig.json'
+        }
+      }),
+
+      // Emit CSS as "files" for other plugins to process. default is true
+      // emitCss: false,
+
+      // Warnings are normally passed straight to Rollup. You can
+      // optionally handle them here, for example to squelch
+      // warnings with a particular code
+      // onwarn: (warning, handler) => {
+      //   // e.g. don't warn on <marquee> elements, cos they're cool
+      //   if (warning.code === 'a11y-distracting-elements') return;
+
+      //   // let Rollup handle all other warnings normally
+      //   handler(warning);
+      // },
+
+      // You can pass any of the Svelte compiler options
+      compilerOptions: {
+        // By default, the client-side compiler is used. You
+        // can also use the server-side rendering compiler
+        // generate: 'ssr',
+        // ensure that extra attributes are added to head
+        // elements for hydration (used with generate: 'ssr')
+        // hydratable: true,
+        // You can optionally set 'customElement' to 'true' to compile
+        // your components to custom elements (aka web elements)
+        // customElement: false
+      }
+    }),
+    typescript()
+    // ...
+  ]
+};
---
+++ src/lib/main.ts
@@ -0,0 +1,13 @@
+import Home from './views/home.svelte';
+import Login from './views/login.svelte';
+import Logout from './views/logout.svelte';
+import Register from './views/register.svelte';
+import Todos from './views/todos.svelte';
+
+export {
+  Home,
+  Login,
+  Logout,
+  Register,
+  Todos
+}
